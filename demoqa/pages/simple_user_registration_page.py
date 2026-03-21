35abdcb80dacdf6942e2f599cfc3f05ed6909bfc
k.stadnyk <stadnyk.ca@gmail.com>
2026-03-21 17:50:26 -0400

    added simple_user_registration_steps

--- demoqa/pages/simple_user_registration_page.py
+++ demoqa/pages/simple_user_registration_page.py
@@ -1,33 +1,29 @@
-from selene import browser, have
-
-from demoqa.data.users import User
+from selene import browser
 
 
 class SimpleUserRegistrationPage:
     def __init__(self):
+        self.url = "https://demoqa.com/text-box"
         self.full_name = browser.element("#userName")
         self.email = browser.element("#userEmail")
         self.current_address = browser.element("#currentAddress")
         self.submit_button = browser.element("#submit")
 
-    def open(self):
-        browser.open("https://demoqa.com/text-box")
+    def fill_full_name(self, value):
+        self.full_name.type(value)
         return self
 
-    def fill_form(self, user: User):
-        self.full_name.type(user.full_name)
-        self.email.type(user.email)
-        self.current_address.type(user.current_address)
+    def fill_email(self, value):
+        self.email.type(value)
+        return self
+
+    def fill_current_address(self, value):
+        self.current_address.type(value)
         return self
 
     def submit(self):
         self.submit_button.click()
-        return self
 
-    def should_have_data(self, user):
-        browser.element("#output #name").should(have.text(user.full_name))
-        browser.element("#output #email").should(have.text(user.email))
-        browser.element("#output #currentAddress").should(have.text(user.current_address))
 
 """        browser.all("#output p").should(
             have.texts(user.full_name, user.email, user.current_address)
