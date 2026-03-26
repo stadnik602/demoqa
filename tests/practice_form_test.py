a815ed4bd5786815822f2a73f79c134252771ce4
k.stadnyk <stadnyk.ca@gmail.com>
2026-03-26 18:31:58 -0400

    Added allure link

--- tests/practice_form_test.py
+++ tests/practice_form_test.py
@@ -9,6 +9,9 @@ from demoqa.data import users
 @allure.label("owner", "k.stadnyk")
 @allure.feature("Registration")
 @allure.story("The user can be registered by filling the registration form")
+@allure.link(
+    "https://demoqa.com/automation-practice-form", name="Automation practice form"
+)
 def test_successes_submit_form_by_page_object_steps(app):
     user = users.user
     (
