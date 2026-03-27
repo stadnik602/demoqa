36913af51a278c95aba47fdac96f4f8ae66bc0f1
stadnik602 <stadnik602@gmail.com>
2026-03-26 20:14:39 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,19 +13,20 @@ BASE_URL = "https://demoqa.com"
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")            # headless режим
+    # options.add_argument("--headless=new")            # headless режим
     options.add_argument("--no-sandbox")              # для CI
     options.add_argument("--disable-dev-shm-usage")   # для CI
     options.add_argument("--disable-gpu")             # иногда нужно
     options.add_argument("--window-size=1920,1080")   # для правильного рендера
 
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+        service=Service(ChromeDriverManager().install(), log_path="chromedriver.log"),
         options=options
     )
 
     browser.config.driver = driver
     browser.config.base_url = BASE_URL
+    browser.config.headless = True
     browser.config.timeout = 6
 
     yield
